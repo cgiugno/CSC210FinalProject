fcb99099ba9aa56fb4a55b3bf18f6fb6a10821ae
cgiugno <67064875+cgiugno@users.noreply.github.com>
2020-12-10 14:57:57 -0500

    First Week--Changes

--- app.py
+++ app.py
@@ -1,36 +1,52 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_bootstrap import Bootstrap
 
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from sqlalchemy import MetaData
 
-from forms import LoginForm, RegisterForm
+from datetime import datetime
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+
+from forms import LoginForm, RegisterForm, EditProfileForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userlists.db'
-app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'login'
-
 application = app
+
+#Initialize Bootstrap, Manager
 bootstrap = Bootstrap(app)
 manager = Manager(app)
 
-#Initialize login
+#Setup SQLDatabase name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Configure Login Settings
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login'
 login_manager.init_app(app)
 
-#Initialize database
+#Initialize the database
 db = SQLAlchemy(app)
 
+#Naming convention to enable batch migrations
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+
 #Add migration to database
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 manager.add_command("db", MigrateCommand)
 
 #Create db User model
@@ -41,6 +57,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password = db.Column(db.String(64), index=True)
     password_hash = db.Column(db.String(128))
+    tasks = db.relationship('Tasks', backref='user', lazy="dynamic")
 
     @property
     def password(self):
@@ -53,9 +70,147 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-@app.route('/', methods=['GET', 'POST'])
+#Create Database model/class
+class Tasks(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    topic = db.Column(db.String(200), nullable=True)
+
+    #Create a function to return a string to return a string when we add something to db
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+#initialize database
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("home.html")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        task_topic = request.form['filter']
+        if task_topic == "custom":
+            new_task = Tasks(content=task_content, user=current_user._get_current_object(), topic=request.form['custom_filter'])
+        else:
+            new_task = Tasks(content=task_content, user=current_user._get_current_object(), topic=task_topic)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/') #for DigScholar this will be 'return redirect('/final')
+        except:
+            return 'There was an issue adding your task :('
+    
+    else:
+        if current_user.is_authenticated:
+            tasks = Tasks.query.filter_by(user=current_user).order_by(Tasks.date_created).all()
+        else:
+            tasks = Tasks.query.order_by(Tasks.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
+    if request.method == 'GET':
+        pass
+
+    return render_template('index.html')
+
+@app.route("/filter/<string:selected_topic>", methods=['POST', 'GET'])
+def filter(selected_topic):
+    filter_topic = selected_topic
+    if request.method == 'POST':
+        task_content = request.form['content']
+        task_topic = selected_topic
+        
+        new_task = Tasks(content=task_content, user=current_user._get_current_object(), topic=task_topic)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            urlStr = '/filter/%s' %selected_topic
+            return redirect(urlStr) #for DigScholar this will be 'return redirect('/final/filter/%s')
+        except:
+            return 'There was an issue adding your task :('
+    
+    else:
+        if filter_topic != "all":
+            tasks = Tasks.query.filter_by(topic=selected_topic).order_by(Tasks.date_created).all() 
+        else:
+            tasks = Tasks.query.order_by(Tasks.date_created).all()
+        return render_template('index.html', tasks=tasks, selected_topic=selected_topic)
+    
+    if request.method == 'GET':
+        pass
+
+    return render_template('index.html')
+    
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Tasks.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/') #for DigScholar this will be 'return redirect('/final')
+
+    except:
+        return 'There was an error deleting that task'
+
+@app.route('/<string:selected_topic>/delete/<int:id>')
+def filter_delete(selected_topic, id):
+    filter_topic = selected_topic
+    task_to_delete = Tasks.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/filter/' +filter_topic) #for DigScholar this will be 'return redirect('/final')
+
+    except:
+        return 'There was an error deleting that task'
+
+@app.route('/<string:selected_topic>/update/<int:id>', methods=['GET','POST'])
+def filter_update(selected_topic, id):
+    task = Tasks.query.get_or_404(id)
+    filter_topic = selected_topic
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+        task_topic = request.form['filter']
+        if task_topic == "custom":
+            task.topic = request.form['custom_filter']
+        else:
+            task.topic = task_topic
+        try:
+            db.session.commit()
+            return redirect('/filter/' +request.form['filter']) #for DigScholar this will be 'return redirect('/final')
+        except:
+            return 'There was an issue updating your task :('
+
+    else:
+        return render_template('update.html', task=task, selected_topic=selected_topic)
+
+@app.route('/update/<int:id>', methods=['GET','POST'])
+def update(id):
+    task = Tasks.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+        task.topic = request.form['filter']
+        if task_topic == "custom":
+            task.topic = request.form['custom_filter']
+        else:
+            task.topic = task_topic
+        try:
+            db.session.commit()
+            return redirect('/') #for DigScholar this will be 'return redirect('/final')
+        except:
+            return 'There was an issue updating your task :('
+
+    else:
+        return render_template('update.html', task=task)
 
 @app.route("/logout", methods=["GET", "POST"])
 @login_required
@@ -81,10 +236,40 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
+            login_user(user, form.remember_user.data)
             return redirect(request.args.get('next') or url_for('index'))
         flash("Invalid username or password.")
     return render_template("login.html", form=form)
+#Current logins are:
+# Email: dustychirpchirp@gmail.com
+# Username: Dusty
+# Password: dusty
+#And
+# Email: fluffyhoneypot@gmail.com
+# Username: Fluffy
+# Password: fluffy
+#(They're both named after my birds -- Katie)
+
+@app.route("/about", methods=["GET","POST"]) 
+@login_required
+def about():
+    return render_template("about.html")
+
+@app.route("/about/edit", methods=["GET", "POST"])
+@login_required
+def about_edit():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        user = current_user._get_current_object()
+        if form.edit_username.data != "":
+            user.username = form.edit_username.data
+        if form.edit_email.data != "":
+            user.email = form.edit_email.data
+        if user.verify_password(form.original_password.data):
+            user.password = form.new_password.data
+        db.session.commit()
+        return redirect(url_for("about"))
+    return render_template("edit_about.html", form=form)
 
 #We can use the @login_required to keep guest users from seeing a page
 @app.route('/secret')
@@ -92,6 +277,11 @@ def login():
 def secret():
     return "Only authenticated users are allowed!"
 
+#Instantiates user
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    manager.run()
--- forms.py
+++ forms.py
@@ -1,14 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField, validators, ValidationError
-from wtforms.validators import InputRequired, Regexp, Length, EqualTo
+from wtforms.validators import InputRequired, Regexp, Length, EqualTo, Optional
 #from app import User
 
+#Login Form 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[InputRequired(), Length(1, 64)])
     password = PasswordField("Password", validators=[InputRequired()])
     remember_user = BooleanField("Keep me logged in!")
     submit = SubmitField("Log In")
 
+#Sign up Form
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[InputRequired(), Length(1, 64)])
     username = StringField("Username", validators=[InputRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Usernames must have only letters, numbers, dots or underscores.')])
@@ -22,3 +24,14 @@ class RegisterForm(FlaskForm):
 #   def validate_username(self, field):
 #       if User.query.filter_by(username=field.data).first():
 #           raise ValidationError("Username already in use.")
+#   Not working for some reason ^^
+
+#Edit Profile Form
+class EditProfileForm(FlaskForm):
+    edit_username = StringField("Edit Username:", validators=[Optional(), Length(1, 64)])
+    original_password = PasswordField("Enter Original Password:", validators=[Optional(), Length(1, 64), EqualTo("repeat_original_password", message="Old passwords must match!")])
+    repeat_original_password = PasswordField("Re-enter Original Password:", validators=[Optional(), Length(1,64)])
+    new_password = PasswordField("Enter New Password:", validators=[Optional(), Length(1, 64), EqualTo("repeat_new_password", message="New passwords must match!")])
+    repeat_new_password = PasswordField("Re-enter New Password:", validators=[Optional(), Length(1, 64)])
+    edit_email = StringField("Edit Email:", validators=[Optional(), Length(1, 64)])
+    submit_button = SubmitField(" Submit Edits ")
